2f4b6a1510d510261551ad4daf24db04f05ec145
haefneryan <90111262+haefneryan@users.noreply.github.com>
2021-10-22 13:12:22 -0400

    Build Structure for Portfolio
    Add resume, cover letter, nav bar

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+
+function homeScroll () {
+    document.getElementById('home').scrollIntoView({block: 'center'});
+}
+
+function projectsScroll () {
+    document.getElementById('projects').scrollIntoView({block: 'center'});
+}
+
+function contactScroll () {
+    document.getElementById('contactInfo').scrollIntoView({block: 'center'});
+}
