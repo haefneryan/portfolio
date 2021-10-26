31bd0a284e5bd3b00eacd603366d09dc01f4f968
haefneryan <90111262+haefneryan@users.noreply.github.com>
2021-10-26 11:26:25 -0400

    Added Logo and about section

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,12 @@
 
 function homeScroll () {
-    document.getElementById('home').scrollIntoView({block: 'center'});
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    //document.getElementById('home').scrollIntoView({block: 'center'});
+}
+
+function aboutScroll () {
+    document.getElementById('about').scrollIntoView({block: 'center'});
 }
 
 function projectsScroll () {
